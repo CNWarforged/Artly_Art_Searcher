ef9b6889b0959dc255708ed45a6b2ddcb3a23ebe
CNWarforged <121924263+CNWarforged@users.noreply.github.com>
2025-05-08 21:23:37 -0700

    tweak to artists, index, dml

--- DML.sql
+++ DML.sql
@@ -9,10 +9,22 @@ SELECT Artists.artistID, Artists.fullName AS 'Name', Artists.genderCode AS 'Gend
             LEFT JOIN Locations ON Artists.birthLocID = Locations.locationID;
 
 -- Query 2 for the Artists table
--- Get all locations from the Locations table to fill in the locations drop downs for the update tables
+-- Get all locations from the Locations table to fill in the locations drop downs for the location update tables
 SELECT * FROM Locations;
 
+------
 
+-- CREATE
+-- add a new artist
+INSERT INTO Artist (fullName, genderCode, queer, residenceLocID, birthLocID) VALUES (@create_artist_fullname, @create_artist_gender, @create_artist_queer, @create_artist_birth_loc, @create_artist_res_loc)
+
+------
+
+-- UPDATE
+-- update an artist's info based on the form choices
+UPDATE Artist SET fullName = @update_artist_name, genderCode = @update_artist_gender, residenceLocID = @update_artist_res_location, birthLocID = @update_artist_birth_location WHERE artistID = @update_artist_id
+
+------
 
 -- DELETE
 -- delete their artworks first
