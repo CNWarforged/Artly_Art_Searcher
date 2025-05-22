9b9cb37a54538ef84a3559161de32b694508c766
CNWarforged <121924263+CNWarforged@users.noreply.github.com>
2025-05-22 15:55:23 -0700

    updated dml

--- DML.sql
+++ DML.sql
@@ -6,86 +6,96 @@
 -- Get all artist IDs, names, gender codes, sexualities, residence IDs, birthplace IDs, the country and state 
 -- Join the locations table to get the details for the location
 SELECT Artists.artistID as 'ID', Artists.fullName AS 'Name', 
-            Artists.genderCode AS 'Gender', Artists.queer AS 'Queer', 
-            Artists.residenceLocID AS 'Residence', 
-            Artists.birthLocID AS 'Birthplace', 
-            Locations.country AS 'Country', Locations.state AS 'State' FROM Artists 
+            GenderCodes.description AS 'Gender', Artists.queer, 
+            CASE 
+            WHEN Artists.queer = 1 THEN 'Yes' 
+            WHEN Artists.queer = 0 THEN 'No' 
+            END AS Queer, 
+            Locations.country AS 'Country', Locations.state AS 'State', Locations.city AS 'City' 
+            FROM Artists 
             LEFT JOIN Locations ON Artists.birthLocID = Locations.locationID 
+            JOIN GenderCodes on Artists.genderCode = GenderCodes.genderID 
             ORDER BY Artists.fullName ASC;
 -- Query 2 for the Artists table
 -- Get all locations from the Locations table to fill in the locations drop downs for the Artist updates
 SELECT * FROM Locations;
---Query 3 for the Artists table
---Get all gender codes from the GenderCodes table to fill in the gender drop downs for the Artist updates
+-- Query 3 for the Artists table
+-- Get all gender codes from the GenderCodes table to fill in the gender drop downs for the Artist updates
 SELECT * FROM GenderCodes;
 
 -- For artworks.hbs
---Get all info from the Artworks table, join on ArtistArtworks and Artists to get the Artist name for the Artworks update table
-SELECT Artworks.artworkID AS 'Artwork_ID', Artworks.digitalArt AS 'Digital_Art', 
-            DATE_FORMAT(Artworks.dateCreated, '%Y-%m-%d') AS 'Date', Artworks.artPeriodCode AS 'Period', 
-            Artworks.artMediumCode AS 'Medium', Artworks.artName AS 'Artwork_Name', 
-            Artists.fullName AS 'Artist_Name' 
+-- Get all info from the Artworks table, join on ArtistArtworks and Artists to get the Artist name for the Artworks update table
+SELECT Artworks.artworkID, Artworks.digitalArt, 
+            CASE 
+            WHEN Artworks.digitalArt = 1 THEN 'Yes' 
+            WHEN Artworks.digitalArt = 0 THEN 'No' 
+            END AS digitalArt, 
+            DATE_FORMAT(Artworks.dateCreated, '%Y-%m-%d') AS dateCreated, ArtPeriods.century, 
+            ArtPeriods.centuryPart, Mediums.mediumDescription, Artworks.artName, 
+            Artists.fullName 
             FROM Artworks 
-            JOIN Artists ON Artists.artistID = (SELECT artistID FROM ArtistArtworks 
-            WHERE ArtistArtworks.artworkID = Artworks.artworkID)
+            JOIN ArtPeriods ON Artworks.artPeriodCode = ArtPeriods.periodID 
+            JOIN Mediums ON Artworks.artMediumCode = Mediums.mediumID 
+            JOIN ArtistArtworks ON Artworks.artworkID = ArtistArtworks.artworkID 
+            JOIN Artists ON ArtistArtworks.artistID = Artists.artistID 
             ORDER BY Artworks.artName ASC;
---Query2 for Artworks to get art periods for the Artwork updates
+-- Query2 for Artworks to get art periods for the Artwork updates
 SELECT * FROM ArtPeriods;
---Query3 for Artworks to get all the medium descriptions for Artwork updates 
+-- Query3 for Artworks to get all the medium descriptions for Artwork updates 
 SELECT * FROM Mediums;
---Query4 for Artworks to get all the Artist names for the Artwork updates
+-- Query4 for Artworks to get all the Artist names for the Artwork updates
 SELECT * FROM Artists;
 
---For artperiods.hbs
---Get all the data from this table but formatted to be easier to read
+-- For artperiods.hbs
+-- Get all the data from this table but formatted to be easier to read
 SELECT periodID AS 'Period', century AS 'Century', centuryPart AS 'Century_Part' FROM ArtPeriods;
 
---for locations.hbs
---Get all the data from this table but formatted to be easier to read
+-- for locations.hbs
+-- Get all the data from this table but formatted to be easier to read
 SELECT locationID AS 'ID', country AS 'Country', state AS 'State', city AS 'City' FROM Locations;
 
---for gendercodes.hbs
---Get all the data from this table but formatted to be easier to read
+-- for gendercodes.hbs
+-- Get all the data from this table but formatted to be easier to read
 SELECT genderID AS 'ID', description AS 'Description' FROM GenderCodes;
 
---for mediums.hbs
---Get all the data from this table but formatted to be easier to read
+-- for mediums.hbs
+-- Get all the data from this table but formatted to be easier to read
 SELECT mediumID AS 'ID', mediumDescription AS 'Description' FROM Mediums;
 
---for artist-summary.hbs
---This selects everything from a table View that counts the number of artworks attached to an artist
+-- for artist-summary.hbs
+-- This selects everything from a table View that counts the number of artworks attached to an artist
 SELECT artistID AS 'ID', fullName AS 'Artist_Name', artworkCount AS 'Artwork_Count' 
             FROM v_artistartcount 
             ORDER BY fullName ASC;
 
---for edit-artist.hbs
---This one requires multiple queries to allow for updates and also drop downs
---It fills the edit-artist form for updates
---It uses a passed id from another form
+-- for edit-artist.hbs
+-- This one requires multiple queries to allow for updates and also drop downs
+-- It fills the edit-artist form for updates
+-- It uses a passed id from another form
 SELECT artistID AS 'ID', fullName AS 'Artist_Name', genderCode AS 'Gender', queer AS 'Queer', 
 			residenceLocID AS 'Residence', birthLocID AS 'Birthplace' 
             FROM Artists WHERE artistID = @artistID;
---Query 2 for this page:
+-- Query 2 for this page:
 SELECT Artworks.artworkID AS 'Artwork_ID', Artworks.artName AS 'Artwork_Name', Artworks.digitalArt AS 'Digital_Art', 
             DATE_FORMAT(Artworks.dateCreated, '%Y-%m-%d') AS 'Date', Artworks.artPeriodCode AS 'Period', 
 			Artworks.artMediumCode AS 'Medium' 
             FROM Artworks 
             JOIN ArtistArtworks ON Artworks.artworkID = ArtistArtworks.artworkID 
             WHERE ArtistArtworks.artistID = @artistID;
---Query 3 for this page:
+-- Query 3 for this page:
 SELECT locationID AS 'Location', country AS 'Country', state AS 'State', city AS 'City'
             FROM Locations;
---Query 4 for this page:
+-- Query 4 for this page:
 SELECT periodID AS 'Period', century AS 'Century', centuryPart AS 'Century_Part'
             FROM ArtPeriods;
---Query 5 for this page:
+-- Query 5 for this page:
 SELECT mediumID AS 'Medium', mediumDescription AS 'Description' 
             FROM Mediums;
---Query 6 for this page:
+-- Query 6 for this page:
 SELECT * FROM GenderCodes;
 
 
-----------------------------------------------
+-- --------------------------------------------
 
 
 -- CREATE --
@@ -102,7 +112,7 @@ INSERT INTO ArtistArtworks (artistID, artworkID)
 VALUES ((SELECT artistID FROM Artists WHERE fullName = @artistName), (SELECT artworkID FROM Artworks WHERE artName = @artName));
 
 
-----------------------------------------------
+-- --------------------------------------------
 
 
 -- UPDATE --
@@ -112,30 +122,37 @@ UPDATE Artists SET artistID = @artistID, fullName = @update_artist_name, genderC
 -- update artwork info based on form choices
 UPDATE Artworks SET artworkID = @artworkID, digitalArt = @digitalArt, dateCreated = @dateCreated, artPeriodCode = @artPeriodCode, artMediumCode = @artMediumCode, artname = @artName;
 
---update ArtistArtworks with new artist ID
+-- update ArtistArtworks with new artist ID
 UPDATE ArtistArtworks SET artistID = @new_artistID WHERE (artistID = @old_artistID);
 
---update ArtistArtworks with new artwork ID
+-- update ArtistArtworks with new artwork ID
 UPDATE ArtistArtworks SET artworkID = @new_artworkID WHERE (artworkID = @old_artworkID);
 
-----------------------------------------------
+-- --------------------------------------------
 
 
 -- DELETE --
---To DELETE an ARTIST: (Without any associated artworks we remove the artist entirely)
+-- To DELETE an ARTIST: (Without any associated artworks we remove the artist entirely)
 -- delete their artworks first
-DELETE FROM Artworks WHERE artworkID = SELECT (artworkID FROM ArtistArtworks WHERE artistID = @delete_artist_id)
+-- These are all stored in sp_delete_artist
+DELETE FROM ArtistArtworks 
+	WHERE artistID = @in_artistID;
 
--- Then delete the artist/artwork connection
-DELETE FROM ArtistArtworks WHERE artistID = @delete_artist_id
+-- Then, delete artworks with no remaining artist links
+DELETE FROM Artworks
+    WHERE artworkID NOT IN (SELECT DISTINCT artworkID FROM ArtistArtworks);
 
--- Finally delete the artist
-DELETE FROM Artists WHERE artistID = @delete_artist_id
+-- Finally, delete the artist
+DELETE FROM Artists
+    WHERE artistID = @in_artistID;
 
 
---To DELETE an ARTWORK: (We'll leave the artist alone in this case)
+-- To DELETE an ARTWORK: (We'll leave the artist alone in this case)
+-- These are all stored in sp_delete_artwork
 -- First delete the artist/artwork connection
-DELETE FROM ArtistArtworks WHERE artworkID = @delete_artwork_id
+DELETE FROM ArtistArtworks
+    WHERE artworkID = @in_artworkID;
 
---Then delete the Artwork
-DELETE FROM Artworks WHERE artworkID = @delete_artwork_id
+    -- Delete artwork from Artworks
+DELETE FROM Artworks
+    WHERE artworkID = @in_artworkID;
